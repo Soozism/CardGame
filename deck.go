2669abb6ac7fd10f5345de3c15a42052f031ec78
Soozism <a.soozism@gmail.com>
2024-07-23 10:51:51 +0330

    Initial commit

---
+++ deck.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+//
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	cards := newDeck()
+
+	cards.print()
+
+}
